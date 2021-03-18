0681f63aba1c0ce97b3b3fe1ba2c4fa5c31acfe5
Nushrat Jahan <nushratjahan296@gmail.com>
2021-03-18 08:56:44 +0600

    admin & teacher

--- Project mid work/app/Http/Controllers/TeacherController.php
+++ Project mid work/app/Http/Controllers/TeacherController.php
@@ -2,13 +2,99 @@
 
 namespace App\Http\Controllers;
 
+use App\Course;
+use App\Http\Requests\TeacherProfileRequest;
+use App\Http\Requests\ProPicRequest;
+use App\Student;
 use Illuminate\Http\Request;
+use App\Teacher;
+use App\TeacherCourse;
 
 class TeacherController extends Controller
 {
-    public function index(Request $req)
+    //
+    public function index(Request $request)
     {
-        return view('teacher.index');
+        $teacher = Teacher::where('username',$request->session()->get('username'))
+                    ->first();
+        $teacherCourse = TeacherCourse::where('teacher_id',$teacher->teacher_id)
+                                       ->where('status','In progress')
+                                       ->get();
+
+        $course = Course::all();
+
+        return view('teacher.index',compact('teacher','course','teacherCourse'));
+    }
+
+    public function profile(Request $request)
+    {
+        $teacher = Teacher::where('username',$request->session()->get('username'))
+                    ->first();
+        return view('teacher.profile',compact('teacher'));
+    }
+    public function profilePicUp(ProPicRequest $request)
+    {
+        $teacher = Teacher::where('username',$request->session()->get('username'))
+                    ->first();
+        if($teacher->profile_pic) {
+            unlink($teacher->profile_pic);
+        }
+        $extension = $request->profile_picture->getClientOriginalExtension();
+        $fileName = date('U').'.'.$extension;
+        $destination = "images/teacher/";
+        $request->profile_picture->move($destination, $fileName);
+        $teacher->profile_pic = $destination.$fileName;
+        $teacher->save();
+        return Back();
+    }
+
+    public function edit(Request $request)
+    {
+        $teacher = Teacher::where('username',$request->session()->get('username'))
+                    ->first();
+        return view('teacher.edit',compact('teacher'));
+    }
+
+    public function profileUpdate(TeacherProfileRequest $request)
+    {
+        $teacher = Teacher::where('username',$request->session()->get('username'))
+                    ->first();
+
+        if($request->profile_pic)
+        {
+            if($teacher->profile_pic) {
+                unlink($teacher->profile_pic);
+            }
+            $extension = $request->profile_picture->getClientOriginalExtension();
+            $fileName = date('U').'.'.$extension;
+            $destination = "images/teacher/";
+            $request->profile_picture->move($destination, $fileName);
+            $teacher->profile_pic = $destination.$fileName;
+
+        }
+
+        $teacher->name = $request->name;
+        $teacher->phone = $request->phone;
+        $teacher->email = $request->email;
+        $teacher->address = $request->address;
+
+        $teacher->save();
+        $request->session()->flash('msg','Profile Updated Sucessfully!');
+        return view('teacher.profile',compact('teacher'));
 
     }
+
+    public function viewStudent(Request $request)
+    {
+        $teacher = Teacher::where('username',$request->session()->get('username'))
+                    ->first();
+        $teacherCourse = TeacherCourse::where('teacher_id',$teacher->teacher_id)
+                                       ->get();
+
+        $course = Course::all();
+        $student = Student::all();
+        return view('teacher.viewStudent',compact('teacher','course','teacherCourse'));
+    }
+
+
 }
--- Project mid work/routes/web.php
+++ Project mid work/routes/web.php
@@ -15,9 +15,83 @@ Route::get('/', function () {
     return redirect('/login');
 });
 
-Route::get('/login','LoginController@index');
+Route::get('/login','LoginController@index')->name('login');
 Route::post('/login','LoginController@verify');
 
+Route::get('logout', 'LogoutController@logout')->name('logout');
+
 Route::get('check','Check@index');
-Route::get('/teacher','TeacherController@index')->name('teacher');
+
+Route::middleware(['sessionVerify'])->group(function () {
+    //------Start of Admin Part
+    Route::get('admin','AdminController@index')->name('admin');
+
+    Route::get('create/admin','AdminController@createAdmin')->name('create.admin');
+    Route::post('create/admin','AdminController@createAdminVerify');
+
+    Route::get('create/account','AdminController@createAccount')->name('create.account');
+    Route::post('create/account','AdminController@createAccountVerify')->name('create.account');
+
+    Route::get('create/teacher','AdminController@createTeacher')->name('create.teacher');
+    Route::post('create/teacher','AdminController@createTeacherVerify');
+
+    Route::get('create/student','AdminController@createStudent')->name('create.student');
+    Route::post('create/student','AdminController@createStudentVerify');
+
+    Route::get('admin/view/universityList','AdminModifyController@viewUniversity')->name('admin.view.university');
+    Route::get('admin/edit/university/{univ_id}','AdminModifyController@editUniversity')->name('admin.edit.university');
+    Route::get('admin/details/university/{univ_id}','AdminModifyController@detailsUniversity')->name('admin.details.university');
+    Route::post('admin/edit/university/{univ_id}','AdminModifyController@editUniversityVerify');
+    
+    Route::get('admin/view/adminList','AdminModifyController@viewAdmin')->name('admin.view.admin');
+    Route::get('admin/details/admin/{ad_id}','AdminModifyController@detailsAdmin')->name('admin.details.admin');
+
+    Route::get('admin/view/accountList','AdminModifyController@viewAccount')->name('admin.view.account');
+    Route::get('admin/edit/account/{ac_id}','AdminModifyController@editAccount')->name('admin.edit.account');
+    Route::post('admin/edit/account/{ac_id}','AdminModifyController@editAccountVerify');
+    Route::get('admin/details/account/{ac_id}','AdminModifyController@detailsAccount')->name('admin.details.account');
+    
+    Route::get('admin/view/teacherList','AdminModifyController@viewTeacher')->name('admin.view.teacher');
+    Route::get('admin/edit/teacher/{t_id}','AdminModifyController@editTeacher')->name('admin.edit.teacher');
+    Route::post('admin/edit/teacher/{t_id}','AdminModifyController@editTeacherVerify');
+    Route::get('admin/details/teacher/{t_id}','AdminModifyController@detailsTeacher')->name('admin.details.teacher');
+
+    Route::get('admin/view/studentList','AdminModifyController@viewStudent')->name('admin.view.student');
+    Route::get('admin/edit/student/{s_id}','AdminModifyController@editStudent')->name('admin.edit.student');
+    Route::post('admin/edit/student/{s_id}','AdminModifyController@editStudentVerify');
+    Route::get('admin/details/student/{s_id}','AdminModifyController@detailsStudent')->name('admin.details.student');
+
+    //--------------------------------------------END OF ADMIN PART
+
+    //------Start of Teacher Part
+
+    Route::get('teacher','TeacherController@index')->name('teacher');
+    Route::get('teacher/profile','TeacherController@profile')->name('teacher.profile');
+    Route::post('teacher/profile','TeacherController@profilePicUP');
+    Route::get('teacher/profile/edit','TeacherController@edit')->name('teacher.edit');
+    Route::post('teacher/profile/edit','TeacherController@profileUpdate');
+    Route::get('teacher/student','TeacherController@viewStudent')->name('teacher.viewStudent');
+
+    Route::get('teacher/course','CourseController@teacherCourselist')->name('teacher.viewCourselist');
+    Route::get('teacher/mycourse','CourseController@teacherCourse')->name('teacher.viewMyCourselist');
+    Route::post('teacher/course/details/{course_id}','CourseController@courseDetails')->name('teacher.courseDetails');
+
+
+
+    //--------------------------------------------END OF TEACHER PART
+
+    //------Start of Accounts Part
+
+    Route::get('account','AccountController@index')->name('account');
+
+    //--------------------------------------------End Of Accounts Part
+
+    //------Start of Student Part
+
+    Route::get('student','StudentController@index')->name('student');
+
+    //--------------------------------------------End Of Student Part
+
+});
+
 
